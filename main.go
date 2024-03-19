17b63e345dcfeddfcde0d1f5bc5fe71f8a686a00
Viezas <dxviezas@gmail.com>
2024-03-19 16:08:36 +0100

    feat(ebitengine): get cursor position on click

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Game struct{}
 
+// Update logical state
 func (g *Game) Update() error {
+	mouseIsClicked := inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft)
+	if mouseIsClicked {
+		fmt.Print("Cursor position is : ")
+		fmt.Print(ebiten.CursorPosition())
+		fmt.Println()
+	}
 	return nil
 }
 
+// Render screen
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "This is the beginning of Game of Life")
 }
@@ -21,7 +32,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	ebiten.SetWindowSize(1920, 1080)
+	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Game of Life")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
