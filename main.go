afd5e87e15aa49a0d9bfdd6243773cd635c4ea1e
Viezas <dxviezas@gmail.com>
2024-03-19 15:05:48 +0100

    feat(ebitengine): add ebitengine to project

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "This is the beginning of Game of Life")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
 
 func main() {
-	fmt.Println("Welcome to Game Of Life!")
+	ebiten.SetWindowSize(1920, 1080)
+	ebiten.SetWindowTitle("Game of Life")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
